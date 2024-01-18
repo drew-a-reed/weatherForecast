d39dc6d83c0ba19761d76976e890c883ffc85a98
Drew Reed <drew.reed@hotmail.com>
2024-01-18 09:59:19 -0600

    changed font color

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  referringUrl: string | null = null;
 
+  ngOnInit() {
+    this.setLinkText();
+  }
+
+  setLinkText() {
+    this.referringUrl = document.referrer;
+    const linkElement = document.querySelector(".header a") as HTMLElement;
+
+    if (linkElement) {
+      const linkText = this.referringUrl === 'http://localhost:55875/'
+        ? 'Return to my profile'
+        : linkElement.textContent || linkElement.innerText;
+
+      console.log('Link Text:', linkText);
+
+      // Your logic for setting link text based on the referring URL
+      // For example, you can update the link element text content
+      linkElement.textContent = linkText;
+    }
+  }
 }
